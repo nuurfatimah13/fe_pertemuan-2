fea81c93675e38718b5e714f1635fda0beb05773
Imah <nuurfatimah13@gmail.com>
2022-03-02 19:14:55 +0700

    task_pertemuan-2

---
+++ task.js
@@ -0,0 +1,135 @@
+/**
+ * My name is Fathimah (Imah)
+ * my id number is 0110220233
+ */
+
+/**
+ * TODO 1.
+ * Buat array of object users (5 users).
+ * Object memiliki property: name, age, major.
+ * Note: Ubah var menggunakan JavaScript Modern.
+ */
+const users = [
+  {
+    name: "Fathimah",
+    age: 19,
+    major: "Informatics"
+  },
+  {
+    name: "Fatchan",
+    age: 19,
+    major: "Informatics"
+  },
+  {
+    name: "Robbani",
+    age: 22,
+    major: "Informatics"
+  },
+  {
+    name: "Aziz",
+    age: 20,
+    major: "Informatics"
+  },
+  {
+    name: "Rifqi",
+    age: 19,
+    major: "Informatics"
+  }
+];
+
+/**
+ * TODO 2
+ * Buat function all: Menampilkan semua data user.
+ * Hint: Gunakan for/for-of.
+ * Note: Ubah function menggunakan arrow function.
+ */
+const all = () => {
+  for (const user of users) {
+    console.log(user);
+  }
+}
+
+/**
+ * TODO 3
+ * Buat function store: Menambahkan user baru.
+ * Hint: Gunakan method push.
+ * Note: Ubah function menggunakan arrow function.
+ */
+const store = (user) => {
+  users.push(user);
+  console.log(users);
+}
+
+/**
+ * TODO 4.
+ * Buat function update: Mengedit data user.
+ * Hint: re-assign array.
+ * Note: Ubah function menggunakan arrow function.
+ */
+const update = (index, user) => {
+  users[index] = user;
+  console.log(users);
+}
+
+/**
+ * TODO 5.
+ * Buat function destroy: Menghapus data user.
+ * Hint: Gunakan method splice.
+ * Note: Ubah function menggunakan arrow function.
+ */
+const destroy = (index) => {
+  users.splice(2,1);
+  console.log(users);
+}
+
+/**
+ * Function main.
+ * Jangan edit atau hapus function main.
+ * Function ini untuk testing task.
+ */
+
+const main = () => {
+  /**
+   * Test function index
+   */
+  console.log("# Get All Users");
+  all();
+
+  /**
+   * Test function store
+   */
+  console.log("# Add New User: Sabiq");
+  const newUser = {
+    name: "Sabiq",
+    age: 20,
+    major: "Informatics",
+  };
+  store(newUser);
+
+  /**
+   * Test function update
+   */
+  console.log("# Edit User: Isfa");
+  const editedIndex = 1;
+  const editedUser = {
+    name: "Isfhani Ghiyath",
+    age: 23,
+    major: "English",
+  };
+  update(editedIndex, editedUser);
+
+  /**
+   * Test function destroy
+   */
+  console.log("# Delete User: Nurul");
+  const deletedIndex = 2;
+  destroy(deletedIndex);
+};
+
+main();
+
+/**
+ * Jangan hapus exports.
+ * Exports ini untuk tujuan testing.
+ */
+module.exports = { users, all, store, update, destroy };
